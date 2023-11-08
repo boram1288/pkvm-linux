9e903250849530e4b593fd37e7f2d55d5ab5b666
Mostafa Saleh <smostafa@google.com>
2023-11-08 15:34:02 +0000

    ANDROID: KVM: arm64: devices: Register assignable devices as moveable
    To prevent assignable devices MMIO from being unmapped under
    pressure, register them as moveable memory.
    
    Unfortunately, as moveable memory is registered very early at boot,
    while devices are registered later after pKVM init and IOMMU driver
    register, we would have some duplicate code parsing assignable
    devices from the device tree.
    
    Bug: 357781595
    Bug: 348382247
    Bug: 463588171
    Bug: 458241298
    Change-Id: I5957c5f7c10f435a970409195825259dc65072ad
    Signed-off-by: Mostafa Saleh <smostafa@google.com>
    (cherry picked from commit 9156436491d6aacff634fb1f34fc4d469e49f110)

--- arch/arm64/include/asm/kvm_pkvm.h
+++ arch/arm64/include/asm/kvm_pkvm.h
@@ -116,6 +116,7 @@ static inline unsigned long pvm_supported_vcpu_features(void)
 enum pkvm_moveable_reg_type {
 	PKVM_MREG_MEMORY,
 	PKVM_MREG_PROTECTED_RANGE,
+	PKVM_MREG_ASSIGN_MMIO,
 };
 
 struct pkvm_moveable_reg {
