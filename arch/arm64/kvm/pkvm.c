hlyytine/linux

ANDROID: KVM: arm64: Implement MEM_RELINQUISH SMCCC hypercall
This allows a protected VM running on PKVM to notify the hypervisor
(and host) that it is returning pages to host ownership.

This commit also squashes the following historical commits:
"ANDROID: KVM: arm64: Strictly check page type in MEM_RELINQUISH hypercall
 Icff3474dc2c975a6c5befe546c5521a05b3bd575"
"ANDROID: KVM: arm64: Avoid unnecessary unmap walk in MEM_RELINQUISH hypercall
 I6fd939556b80d7a9a0731cab36166a652f7a7c6d"
"ANDROID: KVM: arm64: Poison pages on the relinquish path
 I85fd3485308209357bcdeb855de07da227fac0fe"
"ANDROID: KVM: arm64: Fix relinquishing from non-protected
 I26e187c321e0f95b49d75fa625bf487b6476bbd6"

Bug: 357781595
Change-Id: I4644736db04afacd7da4c6f465130c73c2e44b93
Signed-off-by: Keir Fraser <keirf@google.com>

arch/arm64/kvm/pkvm.c
@@ -31,6 +31,8 @@ static unsigned int *hyp_memblock_nr_ptr = &kvm_nvhe_sym(hyp_memblock_nr);
 phys_addr_t hyp_mem_base;
 phys_addr_t hyp_mem_size;
 
+static struct rb_node *find_first_mapping_node(struct rb_root *root, u64 gfn);
+
 static int cmp_hyp_memblock(const void *p1, const void *p2)
 {
 	const struct memblock_region *r1 = p1;
@@ -308,6 +310,37 @@ static int __init finalize_pkvm(void)
 	return 0;
 }
 device_initcall_sync(finalize_pkvm);
+
+void pkvm_host_reclaim_page(struct kvm *host_kvm, phys_addr_t ipa)
+{
+	struct kvm_pgtable *pgt = host_kvm->arch.mmu.pgt;
+	struct pkvm_mapping *mapping = NULL;
+	struct mm_struct *mm = current->mm;
+	u64 gfn = ipa >> PAGE_SHIFT;
+	struct rb_node *node;
+	struct page *page;
+
+	write_lock(&host_kvm->mmu_lock);
+	node = find_first_mapping_node(&pgt->pkvm_mappings, gfn);
+	if (node) {
+		mapping = rb_entry(node, struct pkvm_mapping, node);
+		if (mapping->gfn == gfn) {
+			rb_erase(node, &pgt->pkvm_mappings);
+		} else {
+			mapping = NULL;
+			node = NULL;
+		}
+	}
+	write_unlock(&host_kvm->mmu_lock);
+
+	if (WARN_ON(!mapping))
+		return;
+
+	account_locked_vm(mm, 1, false);
+	page = pfn_to_page(mapping->pfn);
+	unpin_user_pages_dirty_lock(&page, 1, true);
+	kfree(mapping);
+}
 
 static int __init pkvm_firmware_rmem_err(struct reserved_mem *rmem,
 					 const char *reason)

boram1288/pkvm-linux

arch/arm64/kvm/pkvm.c
@@ -464,6 +464,28 @@ int pkvm_pgtable_stage2_init(struct kvm_pgtable *pgt, struct kvm_s2_mmu *mmu,
 	return 0;
 }
 
+void pkvm_host_reclaim_page(struct kvm *kvm, phys_addr_t ipa)
+{
+	struct kvm_pgtable *pgt = kvm->arch.mmu.pgt;
+	struct pkvm_mapping *mapping = NULL;
+	struct mm_struct *mm = current->mm;
+	struct page *page;
+
+	write_lock(&kvm->mmu_lock);
+	mapping = pkvm_mapping_iter_first(&pgt->pkvm_mappings, ipa, ipa + PAGE_SIZE - 1);
+	if (mapping)
+		pkvm_mapping_remove(mapping, &pgt->pkvm_mappings);
+	write_unlock(&kvm->mmu_lock);
+
+	if (WARN_ON(!mapping))
+		return;
+
+	account_locked_vm(mm, 1, false);
+	page = pfn_to_page(mapping->pfn);
+	unpin_user_pages_dirty_lock(&page, 1, true);
+	kfree(mapping);
+}
+
 static int __pkvm_pgtable_stage2_unmap(struct kvm_pgtable *pgt, u64 start, u64 end)
 {
 	struct kvm *kvm = kvm_s2_mmu_to_kvm(pgt->mmu);