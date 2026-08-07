CodeLinaro-mirror/kernel-platform-2-0-os_kernel_common

ANDROID: kvm/vfio: pviommu: Add set config IOCTL
Add KVM_PVIOMMU_SET_CONFIG that allows user space to map an index in
the device SID to a virtual SID.

Change-Id: I276c504573ed2564becd572e57a0b2710a1cf147
Signed-off-by: Mostafa Saleh <smostafa@google.com>

virt/kvm/vfio.c
@@ -354,10 +354,52 @@ static int kvm_vfio_set_file(struct kvm_device *dev, long attr,
 	return -ENXIO;
 }
 
+static int kvm_vfio_pviommu_set_config(struct file *fiommu, struct kvm_vfio_iommu_config *config)
+{
+	int vfio_dev_fd = config->device_fd;
+	struct file *filp;
+	int ret;
+	u32 phys_sid;
+	pkvm_handle_t iommu;
+	struct kvm_pviommu *pviommu = fiommu->private_data;
+	struct device *dev;
+
+	filp = fget(vfio_dev_fd);
+	if (!filp)
+		return -EBADF;
+
+	dev = kvm_vfio_file_get_device(filp);
+	if (!dev) {
+		ret = -ENODEV;
+		goto err_fput;
+	}
+
+	ret = kvm_iommu_device_id(dev, config->sid_idx, &iommu, &phys_sid);
+	if (ret)
+		goto err_fput;
+
+	ret = kvm_call_hyp_nvhe(__pkvm_pviommu_add_vsid, pviommu->dev->kvm, pviommu->fd,
+				iommu, phys_sid, config->vsid);
+
+err_fput:
+	fput(filp);
+	return ret;
+}
+
 static long pviommufd_ioctl(struct file *filp, unsigned int ioctl,
 			    unsigned long arg)
 {
-	return -ENXIO;
+	struct kvm_vfio_iommu_config config;
+
+	switch (ioctl) {
+	case KVM_PVIOMMU_SET_CONFIG:
+		if (copy_from_user(&config, (void *)arg, sizeof(config)))
+			return -EFAULT;
+		return kvm_vfio_pviommu_set_config(filp, &config);
+	default:
+		return -ENXIO;
+	}
+	return 0;
 }
 
 static int pviommufd_release(struct inode *i, struct file *filp)

boram1288/pkvm-linux

Bug: 357781595
Bug: 348382247
Bug: 236685427
Bug: 458241298
(cherry picked from commit 07f6e88462b76f4c1f6f7c9218cd918a32b970a5)

virt/kvm/vfio.c
@@ -349,10 +349,58 @@ static int kvm_vfio_set_file(struct kvm_device *dev, long attr,
 }
 
 #ifdef CONFIG_VFIO_PKVM_IOMMU
+static int kvm_vfio_pviommu_set_config(struct file *fiommu, struct kvm_vfio_iommu_config *config)
+{
+	int vfio_dev_fd = config->device_fd;
+	struct file *filp;
+	int ret;
+	u32 phys_sid;
+	pkvm_handle_t iommu;
+	struct kvm_pviommu *pviommu = fiommu->private_data;
+	struct device *dev;
+
+	filp = fget(vfio_dev_fd);
+	if (!filp)
+		return -EBADF;
+
+	dev = kvm_vfio_file_get_device(filp);
+	if (!dev) {
+		ret = -ENODEV;
+		goto err_fput;
+	}
+
+	ret = kvm_iommu_device_id(dev, config->sid_idx, &iommu, &phys_sid);
+	if (ret)
+		goto err_fput;
+
+	ret = kvm_call_hyp_nvhe(__pkvm_pviommu_add_vsid, pviommu->dev->kvm, pviommu->fd,
+				iommu, phys_sid, config->vsid);
+
+err_fput:
+	fput(filp);
+	return ret;
+}
+
 static long pviommufd_ioctl(struct file *filp, unsigned int ioctl,
 			    unsigned long arg)
 {
-	return -ENXIO;
+	struct kvm_vfio_iommu_config config;
+	__u32 usize;
+
+	switch (ioctl) {
+	case KVM_PVIOMMU_SET_CONFIG:
+		if (copy_from_user(&usize, (void *)arg, sizeof(usize)))
+			return -EFAULT;
+		if (usize < offsetofend(struct kvm_vfio_iommu_config, __reserved))
+			return -EINVAL;
+		if (copy_struct_from_user(&config, sizeof(config), (void *)arg, usize))
+			return -EFAULT;
+
+		return kvm_vfio_pviommu_set_config(filp, &config);
+	default:
+		return -ENXIO;
+	}
+	return 0;
 }
 
 static int pviommufd_release(struct inode *i, struct file *filp)